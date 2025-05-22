ca1aa8625ce471831f2abbb0f4f8224ee782a348
roberto <robertobatista18106@gmail.com>
2025-05-22 07:42:33 -0600

    lol

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { ProductoComponent } from './components/producto/producto.component';
 import { CarritoComponent } from './components/carrito/carrito.component';
 import { InventarioComponent } from './components/inventario/inventario.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-    {path: '', component: ProductoComponent},
+    {path: '', component: LoginComponent},
     {path: 'carrito', component: CarritoComponent},
     {path: "inventario", component: InventarioComponent},
+    {path: 'productos', component: ProductoComponent}
 ];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,35 +1,41 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from '../../services/auth.service'
+import { HttpClient, HttpClientModule  } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, HttpClientModule], // 👈 aquí van los módulos necesarios
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private authService: AuthService) {
+  constructor(private fb: FormBuilder, private authService: AuthService, private http: HttpClient,
+        private router: Router
+  ) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
+      nombre: ['', [Validators.required]],
       password: ['', Validators.required]
     });
   }
 
   onSubmit() {
   if (this.loginForm.valid) {
-    const { email, password } = this.loginForm.value;
+    const { nombre, password } = this.loginForm.value;
 
     this.http.get<any>(`http://localhost:3000/api/user`, {
       params: {
-        nombre: email,        // ← lo estás llamando "nombre" en tu backend
+        nombre: nombre,        // ← lo estás llamando "nombre" en tu backend
         password: password
       }
     }).subscribe({
       next: (res) => {
         console.log('Login exitoso:', res);
+        //alert('Login exitoso');
+        this.router.navigate(['/productos']);
         // Puedes guardar el usuario o token aquí
       },
       error: (err) => {
